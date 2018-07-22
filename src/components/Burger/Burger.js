5f20d874e9ac8c9763260dc64d2b7b060cee2d0f
Sebastian Biehl <sebastian-biehl@gmx.net>
2018-07-22 17:33:53 +0200

    added state and dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,10 +4,18 @@ import classes from './Burger.css'
 import Ingredient from './Ingredient/Ingredient'
 
 const burger = props => {
+    
+    const ingredients = Object.keys(props.ingredients)
+        .map(ingredient => {
+            return [...Array(props.ingredients[ingredient])]
+                .map( (_, i) => {
+                    return <Ingredient key={ingredient + i} type={ingredient} />
+                })
+        })
+
     return <div className={classes.Burger}>
         <Ingredient type="bread-top" />
-        <Ingredient type="cheese" />
-        <Ingredient type="meat" />
+        {ingredients}
         <Ingredient type="bread-bottom" />
       </div>;
 };
