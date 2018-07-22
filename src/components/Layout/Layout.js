f5bb63f34cf01e3a186cadb531e64b317fbf036f
Sebastian Biehl <sebastian-biehl@gmx.net>
2018-07-22 16:44:54 +0200

    Burger Builder Container integrated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Wrap from '../../hoc/Wrap'
+import classes from './Layout.css'
 
 const layout = ( props ) => (
     <Wrap>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
     </Wrap>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
+import Wrap from '../../hoc/Wrap'
+
+
 class BurgerBuilder extends Component {
     render () {
         return (
-            
+          <Wrap>
+            <div>Burger</div>
+            <div>Build Controls</div>
+          </Wrap>  
         )
     }
 }
+
+export default BurgerBuilder
