8a62da309908ddb2ec693f172a550ec2d8f0f1c6
Sebastian Biehl <sebastian-biehl@gmx.net>
2018-07-22 18:13:05 +0200

    add and remove ingredients implemented

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,12 +11,19 @@ const controls = [
 ]
 
 const buildControls = props => (
-    <div className={classes.BuildControls}>
-        {controls.map((ctrl) => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
-        ))}
-    </div>
-)
+  <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        more={() => props.ingredientAdded(ctrl.type)}
+        less={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
+);
 
 
 export default buildControls;
