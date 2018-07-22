5fb6fccfb71dcc47a871540c395bcc42ea2e419f
Sebastian Biehl <sebastian-biehl@gmx.net>
2018-07-22 19:39:03 +0200

    Toolbar Navigation added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import Wrap from '../../hoc/Wrap'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = ( props ) => (
     <Wrap>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.content}>
             {props.children}
         </main>
